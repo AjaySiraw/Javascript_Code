1ad8aa8d5e5ffc9562a648e6af325be3979357c8
Ajay Siraw <134132344+AjaySiraw@users.noreply.github.com>
2025-05-18 06:42:34 +0000

    datatypes

---
+++ first_programmes/data_types.js
@@ -0,0 +1,29 @@
+"use strict" //will treat the whole code under new js
+let name="Ajay" //will declare a variable
+const dob="15/10/2002" //will declare a memory component which remains constant
+var sex="male" // declare variable
+
+//use let for most case at present
+
+//use mdn plus for documentation
+
+// alert(3+3) //not a valid syntax for node js  only works in browser
+
+/* 
+  Data types
+  1> number => 2 power53 
+  2> bigint
+  3> boolean
+  4>null =>standalone value
+  5> undefined => when variable not intialized at time of declaration
+  6> string ="" or ''
+  7> symbol => use to define uniquness 
+
+  8> object
+ */
+
+  /*importat function
+  1> typeof 26 =>number // gives the type of variable
+  ** typeof null =>object
+  **typeof undefined =>undefined
+  */
