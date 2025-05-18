352aa55d2395a194cc65619b7cce689e15aaa67e
Ajay Siraw <134132344+AjaySiraw@users.noreply.github.com>
2025-05-18 06:55:11 +0000

    conversions

---
+++ first_programmes/datatype_conversion.js
@@ -0,0 +1,14 @@
+"use strict"
+let age=23
+console.log(age)
+let names="ajay"
+console.log(names)
+ //convert string to number
+ console.log(Number(names))
+ //since ajay cant be a number so it shows NaN but  by conversion we changed its datatype to number
+ //some say its a bug of this language 
+ /*
+ Boolean()=> use to convert in boolean
+ String()=> to string
+ 
+ */
